a2e4d60de930aa8ac842b4ef42d66358d6c0b305
prog037 <asd0430.lim.empty@gmail.com>
2024-07-12 23:47:15 +0900

    FB ２回目修正

--- src/js/script.js
+++ src/js/script.js
@@ -9,12 +9,12 @@ jQuery(function ($) {
       $(".header-drawer__menu").addClass("open");
     }
     // ウィンドウのリサイズを監視し、幅が768px以上の場合はドロワーメニューを閉じる
-  $(window).on("resize", function () {
-    if ($(window).width() > 767) {
-      $(".header__drawer").removeClass("active");
-      $(".header-drawer__menu").removeClass("open");
-    }
-  });
+    $(window).on("resize", function () {
+      if ($(window).width() > 767) {
+        $(".header__drawer").removeClass("active");
+        $(".header-drawer__menu").removeClass("open");
+      }
+    });
   });
 });
 
@@ -23,18 +23,13 @@ jQuery(function ($) {
 $(function () {
   // ハンバーガーメニューボタンがクリックされたときのイベントハンドラを設定
   $(".header__drawer").click(function () {
-
     // 現在のbodyタグのoverflowスタイルを確認
     if ($("body").css("overflow") === "hidden") {
-
       // もしoverflowがhiddenなら、bodyのスタイルを元に戻す
       $("body").css({ height: "", overflow: "" });
-
     } else {
-
       // そうでなければ、bodyにheight: 100%とoverflow: hiddenを設定し、スクロールを無効にする
       $("body").css({ height: "100%", overflow: "hidden" });
-
     }
   });
 });
@@ -52,7 +47,6 @@ const swiper2 = new Swiper(".mySwiper2", {
     delay: 2500, // 2.5秒後に次のスライド
   },
 });
-
 
 /* .campaign
 -------------------------------------------------------------*/
@@ -91,47 +85,45 @@ $(function () {
       pageTop.fadeOut();
     }
   });
-  opacity:1,
-  
-  pageTop.click(function () {
-    $("body,html").animate(
-      {
-        scrollTop: 0,
-      },
-      50
-    );
-    return false;
-  });
+  opacity: 1,
+    pageTop.click(function () {
+      $("body,html").animate(
+        {
+          scrollTop: 0,
+        },
+        50
+      );
+      return false;
+    });
 });
-
-
 
 //要素の取得とスピードの設定
-var box = $('.colorbox'),
-    speed = 700;  
- 
-//.colorboxの付いた全ての要素に対して下記の処理を行う
-box.each(function(){
-    $(this).append('<div class="color"></div>')
-    var color = $(this).find($('.color')),
-    image = $(this).find('img');
-    var counter = 0;
- 
-    image.css('opacity','0');
-    color.css('width','0%');
-    //inviewを使って背景色が画面に現れたら処理をする
-    color.on('inview', function(){
-        if(counter == 0){
-　　　　　$(this).delay(200).animate({'width':'100%'},speed,function(){
-                   image.css('opacity','1');
-                   $(this).css({'left':'0' , 'right':'auto'});
-                   $(this).animate({'width':'0%'},speed);
-                })
-            counter = 1;
-          }
-     });
-});
+var box = $(".colorbox"),
+  speed = 700;
 
+//.colorboxの付いた全ての要素に対して下記の処理を行う
+box.each(function () {
+  $(this).append('<div class="color"></div>');
+  var color = $(this).find($(".color")),
+    image = $(this).find("img");
+  var counter = 0;
+
+  image.css("opacity", "0");
+  color.css("width", "0%");
+  //inviewを使って背景色が画面に現れたら処理をする
+  color.on("inview", function () {
+    if (counter == 0) {
+      $(this)
+        .delay(200)
+        .animate({ width: "100%" }, speed, function () {
+          image.css("opacity", "1");
+          $(this).css({ left: "0", right: "auto" });
+          $(this).animate({ width: "0%" }, speed);
+        });
+      counter = 1;
+    }
+  });
+});
 
 // loading animation
 $(document).ready(function () {
@@ -140,31 +132,27 @@ $(document).ready(function () {
   const totalSlides = leftSlides.length;
   let currentIndex = 0;
 
-
   // 最初のスライドを表示
   $(leftSlides[currentIndex]).addClass("active");
   $(rightSlides[currentIndex]).addClass("active");
-
 });
 
 /* .loading fadeout
--------------------------------------------------------------*/// script.js
+-------------------------------------------------------------*/ // script.js
 
-$(document).ready(function() {
+$(document).ready(function () {
   // フェードアウトさせる要素を取得
-  var $targetElement = $('.fade-out-target');
+  var $targetElement = $(".fade-out-target");
 
   // 指定の秒数（ミリ秒）後にフェードアウトさせる
   var delay = 3000; // 3000ミリ秒（3秒）
 
-  setTimeout(function() {
-    $targetElement.addClass('fade-out');
+  setTimeout(function () {
+    $targetElement.addClass("fade-out");
 
     // フェードアウト後に非表示にする
-    setTimeout(function() {
-      $targetElement.hide();
-    }, 7000); // フェードアウトアニメーションの時間と同じにする
+    setTimeout(function () {
+      $targetElement.fadeOut();
+    }, 2500); // フェードアウトアニメーションの時間と同じにする
   }, delay);
 });
-
-
